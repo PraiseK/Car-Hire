9c7dbdbad8b6eab879cbb4b5d939fd979f588456
PhucLan34 <mitthong12@gmail.com>
2024-06-17 13:23:25 +1000

    assemblyinfo added

--- CarHire/obj/Debug/net8.0/CarHire.AssemblyInfo.cs
+++ CarHire/obj/Debug/net8.0/CarHire.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarHire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d16f1193d2b3d271bbb54cfbccf366f9e4ec30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+961ed354d0396ba3ab412471b8a006ecbbaaa9e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarHire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarHire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CarHire/obj/Release/net8.0/CarHire.AssemblyInfo.cs
+++ CarHire/obj/Release/net8.0/CarHire.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarHire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64859af53693a1523d2991d6dbfae93ce6d06b74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+961ed354d0396ba3ab412471b8a006ecbbaaa9e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarHire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarHire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
