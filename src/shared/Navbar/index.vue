c4820ce16cac51fb55275b0df5cebb046f42d77e
xNart99 <kiradz1328@gmail.com>
2024-06-20 22:42:48 +1000

    Homepage, Nav, AvailableCar Reponsives UI

--- src/shared/Navbar/index.vue
+++ src/shared/Navbar/index.vue
@@ -1,25 +1,66 @@
 <template>
-  <div class="bg-gray-200">
-    <div class="flex justify-between container mx-auto py-2">
-      <div class="flex items-center">
-        <img src="../../assets/images/logo.png" width="100" alt="logo" />
+  <header
+    class="sticky inset-0 z-50 border-b border-slate-100 bg-gray-200 backdrop-blur-lg"
+  >
+    <nav
+      class="mx-auto flex max-w-6xl gap-4 transition-all duration-200 ease-in-out lg:px-12"
+    >
+      <div class="relative flex items-center">
+        <a href="/">
+          <img
+            src="../../assets/images/logo.png"
+            loading="lazy"
+            style="color: transparent"
+            width="100"
+            height="40"
+        /></a>
         <div class="font-bold text-xl">
           Sikuta <br />
           Australia
         </div>
       </div>
-      <div class="flex gap-3 items-center font-semibold">
-        <router-link to="/home">HOME</router-link>
-        |
-        <router-link to="/about">ABOUT US</router-link>
-        <div
-          class="bg-[#dc143c] text-white p-4 rounded-md cursor-pointer hover:bg-[#902c40]"
+
+      <div class="flex-grow"></div>
+      <ul class="hidden items-center justify-center gap-6 md:flex">
+        <li
+          class="font-dm text-sm font-medium text-slate-700 ease-in-out hover:scale-[1.03]"
         >
-          Login / Sign up
-        </div>
+          <router-link to="/home">HOME</router-link>
+        </li>
+        <li
+          class="font-dm text-sm font-medium text-slate-700 ease-in-out hover:scale-[1.03]"
+        >
+          <router-link to="/about">ABOUT US</router-link>
+        </li>
+      </ul>
+      <div class="hidden items-center justify-center gap-6 lg:pr-12 md:flex">
+        <a
+          href="#"
+          class="rounded-md bg-gradient-to-br from-[#c97284] to-[#dc143c] px-3 py-1.5 font-dm text-sm font-medium text-white shadow-md shadow-[#dc143c] transition-transform duration-200 ease-in-out hover:scale-[1.03]"
+          >Sign up
+        </a>
       </div>
-    </div>
-  </div>
+      <div class="relative flex items-center justify-center pr-5 md:hidden">
+        <button type="button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            aria-hidden="true"
+            class="h-6 w-auto text-slate-900"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script setup>
