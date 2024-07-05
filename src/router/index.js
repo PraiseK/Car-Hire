074977b57237650e5a229d6a80d61b09ae73a690
PraiseK <107027405+PraiseK@users.noreply.github.com>
2024-07-06 09:58:30 +1000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,52 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const path = {
-  ROOT: "/home",
-  ABOUT: "/about",
-  AVAILABLECAR: "/available-car",
-  MANAGEBOOKING: "/manage-booking",
-  LOGIN: "/login"
+  ROOT: '/home',
+  ABOUT: '/about',
+  AVAILABLECAR: '/available-car',
+  MANAGEBOOKING: '/manage-booking',
+  LOGIN: '/login'
 };
 
 const routes = [
   {
-    path: "/",
+    path: '/',
     redirect: path.ROOT,
   },
   {
-    path: "/",
-    name: "LayoutCommon",
-    component: () => import("../shared/LayoutCommon/LayoutCommon.vue"),
+    path: '/',
+    name: 'LayoutCommon',
+    component: () => import('../shared/LayoutCommon/LayoutCommon.vue'),
     children: [
       {
         path: path.ROOT,
-        component: () => import("../components/HomePage/HomePage.vue"),
+        component: () => import('../components/HomePage/HomePage.vue'),
       },
       {
         path: path.ABOUT,
-        component: () => import("../components/AboutPage/AboutPage.vue"),
+        component: () => import('../components/AboutPage/AboutPage.vue'),
       },
       {
         path: path.AVAILABLECAR,
         component: () =>
-          import("../components/AvailableCarPage/AvailableCarPage.vue"),
+          import('../components/AvailableCarPage/AvailableCarPage.vue'),
       },
       {
         path: path.MANAGEBOOKING,
         component: () =>
-          import("../components/ManageBooking/ManageBooking.vue"),
+          import('../components/ManageBooking/ManageBooking.vue'),
       },
       {
         path: path.LOGIN,
-        component: () => import("../components/LoginPage/LoginPage.vue"),
+        component: () => import('../components/LoginPage/LoginPage.vue'),
       },
     ],
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory("/"),
+  history: createWebHistory('/'),
   routes,
 });
 
