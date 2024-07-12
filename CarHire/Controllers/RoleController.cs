90f4e235f7a93c07e3e3ef852008d68d8a162415
PhucLan34 <mitthong12@gmail.com>
2024-07-12 21:31:23 +1000

    RoleController

---
+++ CarHire/Controllers/RoleController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CarHire.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        public RoleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select RoleId, RoleName from dbo.Role";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CarHireDBCon2");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
